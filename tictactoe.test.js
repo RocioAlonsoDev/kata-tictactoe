edd3758e43ab1f24a846197e320e536cc80deb45
Rocio <rocioalonso.dev@gmail.com>
2023-08-19 20:03:27 +0200

    first test set, failed

---
+++ tictactoe.test.js
@@ -0,0 +1,18 @@
+const TicTacToe = require('./tictactoe.js');
+
+describe('Tic Tac Toe game testing',()=>{
+    test('When a button is clicked, that cell is assigned to a player',
+    ()=>{
+        //setup
+        const newGame = new TicTacToe();
+        const selectedCell = 2;
+
+        //act
+        newGame.fillCell(selectedCell);
+        let player = newGame.getPlayerX();
+        const expectedResult = [2];
+
+        //compare
+        expected(player).toBe(expectedResult);
+    })
+})
