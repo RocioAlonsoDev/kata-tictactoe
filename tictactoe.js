665686c61540a898f62d7320c4d445d5332e41d2
Rocio <rocioalonso.dev@gmail.com>
2023-08-20 18:47:39 +0200

    second test success

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,19 @@
 class TicTacToe{
     playerX = [];
+    turn = true;
 
     getPlayerX(){
         return this.playerX;
     }
-
+    getTurn(){
+        return this.turn;
+    }
+    setTurn(){
+        this.turn=!this.turn;
+    }
     fillCell(cell){
-         this.playerX.push(cell);
+        this.playerX.push(cell);
+        this.setTurn();
     }
 }
 
